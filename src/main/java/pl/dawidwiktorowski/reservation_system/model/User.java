ad6da393ff72c2cbf1612aee1324a9a1422f5483
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-10-03 23:21:47 +0200

    Created controller, config, model, repository, service package and create user model

---
+++ src/main/java/pl/dawidwiktorowski/reservation_system/model/User.java
@@ -0,0 +1,13 @@
+package pl.dawidwiktorowski.reservation_system.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
