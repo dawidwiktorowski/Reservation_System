03cd8bbae83b8da14b6f0343be2f1c0141def7c6
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-11-19 11:27:04 +0100

    Added repo user, role, category, subcategory, reservation

--- src/main/java/pl/dawidwiktorowski/reservation_system/repository/CategoryRepository.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.dawidwiktorowski.reservation_system.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+
 }
--- src/main/java/pl/dawidwiktorowski/reservation_system/repository/SubCategoryRepository.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/repository/SubCategoryRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.dawidwiktorowski.reservation_system.model.SubCategory;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+
 }
