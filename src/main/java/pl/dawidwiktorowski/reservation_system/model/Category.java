df3370c52c4280d8169a35b6a327db7833c2e050
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-11-20 23:18:58 +0100

    Added category complete entity

--- src/main/java/pl/dawidwiktorowski/reservation_system/model/Category.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/model/Category.java
@@ -1,6 +1,7 @@
 package pl.dawidwiktorowski.reservation_system.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -10,4 +11,40 @@ public class Category {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<SubCategory> subcategories;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, List<SubCategory> subcategories) {
+        this.id = id;
+        this.name = name;
+        this.subcategories = subcategories;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SubCategory> getSubcategories() {
+        return subcategories;
+    }
+
+    public void setSubcategories(List<SubCategory> subcategories) {
+        this.subcategories = subcategories;
+    }
 }
