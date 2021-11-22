5466b33e68be4892c46b9c483858d1a12b15e5ae
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-11-22 23:31:20 +0100

    Test controller

--- src/main/java/pl/dawidwiktorowski/reservation_system/controller/ReservationController.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/controller/ReservationController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.dawidwiktorowski.reservation_system.model.User;
 import pl.dawidwiktorowski.reservation_system.repository.UserRepository;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/reservation")
 public class ReservationController {
     private UserRepository userRepository;
 
@@ -24,4 +26,5 @@ public class ReservationController {
         model.addAttribute("users",users);
         return "index";
     }
+
 }
