90b6af6d96f5f80357aa4af5b628103239776c20
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-11-30 20:38:26 +0100

    Added securityConfig

---
+++ src/main/java/pl/dawidwiktorowski/reservation_system/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package pl.dawidwiktorowski.reservation_system.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder
+                .jdbcAuthentication();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and()
+                .csrf().disable();
+    }
+
+    public void configure(WebSecurity webSecurity){
+        webSecurity.ignoring().antMatchers("/css/**");
+        webSecurity.ignoring().antMatchers("/js/**");
+        webSecurity.ignoring().antMatchers("/images/**");
+    }
+}
