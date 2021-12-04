b683bcff204da333434b1f9db1f85677d77a17ba
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-04 13:26:32 +0100

    Updated security config

--- src/main/java/pl/dawidwiktorowski/reservation_system/config/SecurityConfig.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/config/SecurityConfig.java
@@ -2,42 +2,57 @@ package pl.dawidwiktorowski.reservation_system.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder
-                .jdbcAuthentication();
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select email, password, active from users where email = ?")
+                .authoritiesByUsernameQuery("select u.email, r.role_type, from users u inner join user_role ur on (u.id = ur.user_id) inner join role r on(ur.role_id=r.id) where u.email=?")
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/login").permitAll()
-                .antMatchers("/admin").hasAuthority("ADMIN")
+                .antMatchers("/", "/login").permitAll()
+                .antMatchers("/forget", "/reset").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .usernameParameter("email")
                 .passwordParameter("password")
-                .defaultSuccessUrl("/",true)
+                .defaultSuccessUrl("/", true)
                 .and()
                 .logout()
-                .logoutUrl("/logout")
+                .logoutUrl("/?logout")
                 .logoutSuccessUrl("/");
     }
 
-    public void configure(WebSecurity webSecurity){
+    public void configure(WebSecurity webSecurity) {
         webSecurity.ignoring().antMatchers("/css/**");
         webSecurity.ignoring().antMatchers("/js/**");
         webSecurity.ignoring().antMatchers("/images/**");
