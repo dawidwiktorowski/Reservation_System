f60aa360b0f3dc6eb9ef05261c8b86d7b050c748
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-04 13:08:36 +0100

    Added save user, find by email, find by id, update and forgotten password methods

--- src/main/java/pl/dawidwiktorowski/reservation_system/service/impl/UserServiceImpl.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package pl.dawidwiktorowski.reservation_system.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.dawidwiktorowski.reservation_system.model.Role;
 import pl.dawidwiktorowski.reservation_system.model.User;
 import pl.dawidwiktorowski.reservation_system.repository.RoleRepository;
 import pl.dawidwiktorowski.reservation_system.repository.UserRepository;
@@ -22,30 +23,42 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
-
     @Override
     public void saveUser(User user) {
-
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setActive(true);
+        Role role = roleRepository.findByRoleType("ROLE_USER");
+        user.setRole(role);
+        userRepository.save(user);
     }
 
     @Override
     public User findByEmail(String email) {
-        return null;
+        return userRepository.findByEmail(email);
     }
 
     @Override
     public User findById(Long userId) {
-        return null;
+        return userRepository.findById(userId).orElse(null);
     }
 
     @Override
     public void update(User user) {
-
+        userRepository.findById(user.getId()).ifPresent(updateUser ->{
+            updateUser.setFirstname(user.getFirstname());
+            updateUser.setLastname(user.getLastname());
+            updateUser.setEmail(user.getEmail());
+            updateUser.setPhoneNumber(user.getPhoneNumber());
+            userRepository.save(updateUser);
+        });
     }
 
     @Override
     public void forgottenPassword(User user, Map<String, String> requestParam) {
+        userRepository.findById(user.getId()).ifPresent(updateUserPassword ->{
+            updateUserPassword.setPassword(passwordEncoder.encode(requestParam.get("password")));
+            userRepository.save(updateUserPassword);
+        });
 
     }
 }
