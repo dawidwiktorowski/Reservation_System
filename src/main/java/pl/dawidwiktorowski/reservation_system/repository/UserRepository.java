503427a43125484410d2a1644c533853560d6c4d
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-08 20:30:28 +0100

    Added optional

--- src/main/java/pl/dawidwiktorowski/reservation_system/repository/UserRepository.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/repository/UserRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.dawidwiktorowski.reservation_system.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional <User> findByEmail(String email);
 
 }
