7a673edd63edc7f6f7d1de5cc22ec7abc7cb9b30
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-13 19:51:04 +0100

    Deleted

--- src/main/java/pl/dawidwiktorowski/reservation_system/config/WebSecurityConfig.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/config/WebSecurityConfig.java
@@ -1,35 +1,24 @@
 package pl.dawidwiktorowski.reservation_system.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
+import pl.dawidwiktorowski.reservation_system.service.impl.AppUserService;
 
 @Configuration
+@AllArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    @Autowired
-    private DataSource dataSource;
-    @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("select email, password, active from user where email = ?")
-                .authoritiesByUsernameQuery("select u.email, r.role_type, from users u inner join user_role ur on (u.id = ur.user_id) inner join role r on(ur.role_id=r.id) where u.email=?")
-                .passwordEncoder(bCryptPasswordEncoder);
-    }
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,7 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/login").permitAll()
                 .antMatchers("/forget", "/reset").permitAll()
-                .antMatchers("/register").permitAll()
+                .antMatchers("/signUp").permitAll()
+                .antMatchers("/confirm").permitAll()
+                .antMatchers("/api/v*/registration/**").permitAll()
                 .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
                 .and()
@@ -57,5 +48,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         webSecurity.ignoring().antMatchers("/css/**");
         webSecurity.ignoring().antMatchers("/js/**");
         webSecurity.ignoring().antMatchers("/images/**");
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
     }
 }
--- src/main/java/pl/dawidwiktorowski/reservation_system/controller/AdminController.java
+++
@@ -1,7 +0,0 @@
-package pl.dawidwiktorowski.reservation_system.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class AdminController {
-}
