d3c8c01dc974197632fe97485308345f6f0cf338
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-15 23:04:39 +0100

    Added Category entity

---
+++ src/main/java/pl/dawidwiktorowski/reservation_system/model/Category.java
@@ -0,0 +1,23 @@
+package pl.dawidwiktorowski.reservation_system.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<SubCategory> subCategoryList;
+}
