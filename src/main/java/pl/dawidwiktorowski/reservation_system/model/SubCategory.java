c504e71bfc8cdc6d06e741e2d269726c5a09513c
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-16 08:23:42 +0100

    Added subcategory entity

---
+++ src/main/java/pl/dawidwiktorowski/reservation_system/model/SubCategory.java
@@ -0,0 +1,30 @@
+package pl.dawidwiktorowski.reservation_system.model;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+@Entity
+public class SubCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    @OneToMany(mappedBy = "subCategory")
+    private List<Reservation> reservations;
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+}
