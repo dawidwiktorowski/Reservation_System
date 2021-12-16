694e3ae3708000bcb9cd988e8ff9d960a546bc67
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-16 20:52:15 +0100

    Updated CategoryServiceImplementation

--- src/main/java/pl/dawidwiktorowski/reservation_system/service/impl/CategoryServiceImplementation.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/service/impl/CategoryServiceImplementation.java
@@ -4,24 +4,27 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.dawidwiktorowski.reservation_system.model.Category;
 import pl.dawidwiktorowski.reservation_system.repository.CategoryRepository;
+import pl.dawidwiktorowski.reservation_system.service.CategoryServiceInterface;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class CategoryServiceImplementation {
+public class CategoryServiceImplementation  implements CategoryServiceInterface {
 
     private CategoryRepository categoryRepository;
 
-
+    @Override
     public void add(Category category) {
         categoryRepository.save(category);
     }
 
-    public List<Category> findByAll() {
-        return categoryRepository.findAll();
+    @Override
+    public List<Category> findAll() {
+        return null;
     }
 
+    @Override
     public Category getById(Long id) {
         return categoryRepository.findById(id).orElse(null);
     }
