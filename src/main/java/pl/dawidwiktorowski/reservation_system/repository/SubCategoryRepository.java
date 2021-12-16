dc2e2ded7265797e5f841ccb71462fed63494d32
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-16 20:21:22 +0100

    Added subcategory repository

--- src/main/java/pl/dawidwiktorowski/reservation_system/repository/SubCategoryRepository.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/repository/SubCategoryRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.dawidwiktorowski.reservation_system.model.SubCategory;
 
+import java.util.List;
+
 @Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+    List<SubCategory> findAllByCategoryId(Long id);
 }
