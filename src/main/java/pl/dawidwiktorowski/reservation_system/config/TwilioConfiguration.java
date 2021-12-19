2bf42a28a4c18e3f48cec9f06df60c1cf0eef0b4
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-19 10:59:32 +0100

    Added accountSid, authToken and trialNumber configuration

--- src/main/java/pl/dawidwiktorowski/reservation_system/config/TwilioConfiguration.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/config/TwilioConfiguration.java
@@ -1,4 +1,17 @@
 package pl.dawidwiktorowski.reservation_system.config;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "twilio")
+@Getter
+@RequiredArgsConstructor
 public class TwilioConfiguration {
+    private String accountSid;
+    private String authToken;
+    private String trialNumber;
 }
