6efd02b10ed4ccb0661e48ad3678eb0b814cd7da
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-20 22:18:30 +0100

    Added reservation request email send

--- src/main/java/pl/dawidwiktorowski/reservation_system/service/EmailSender.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/service/EmailSender.java
@@ -1,5 +1,14 @@
 package pl.dawidwiktorowski.reservation_system.service;
 
+import pl.dawidwiktorowski.reservation_system.model.AppUser;
+import pl.dawidwiktorowski.reservation_system.model.SubCategory;
+
+import java.time.LocalDateTime;
+
 public interface EmailSender {
-    void send (String to, String email);
+    void send(String to, String email);
+
+    void reservationRequestEmailSend(AppUser appUser, LocalDateTime dateTime, SubCategory subCategory);
+
+    void sendEmailWhenUserUpdateReservation(AppUser appUser, LocalDateTime localDateTime, SubCategory subCategory);
 }
