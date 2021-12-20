5ff271a2669daf40da608c52e72ec55bd9c121e8
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-20 08:15:48 +0100

    Added twilio and email service to ReservationServiceImplementation

--- src/main/java/pl/dawidwiktorowski/reservation_system/service/impl/ReservationServiceImplementation.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/service/impl/ReservationServiceImplementation.java
@@ -1,13 +1,15 @@
 package pl.dawidwiktorowski.reservation_system.service.impl;
 
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.dawidwiktorowski.reservation_system.config.TwilioConfiguration;
 import pl.dawidwiktorowski.reservation_system.model.AppUser;
 import pl.dawidwiktorowski.reservation_system.model.Reservation;
 import pl.dawidwiktorowski.reservation_system.repository.ReservationRepository;
 import pl.dawidwiktorowski.reservation_system.service.ReservationServiceInterface;
 
-import javax.mail.Message;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -17,12 +19,17 @@ public class ReservationServiceImplementation implements ReservationServiceInter
 
     private ReservationRepository reservationRepository;
 
+    private TwilioConfiguration twilioConfiguration;
+
+    private final EmailService emailService;
+
 
     @Override
     public void save(Reservation reservation, AppUser appUser) {
         reservation.setStatus(false);
         reservation.setAppUser(appUser);
         reservationRepository.save(reservation);
+
     }
 
     @Override
@@ -35,6 +42,12 @@ public class ReservationServiceImplementation implements ReservationServiceInter
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm a");
         reservationRepository.findById(id).ifPresent(x -> {
             x.setStatus(true);
+            Message.creator(
+                    new PhoneNumber(x.getAppUser().getPhoneNumber()),
+                    new PhoneNumber(twilioConfiguration.getTrialNumber()),
+                    "Twoja rezerwacja na : " + x.getSubCategory().getName() + " w dniu "
+                    + dateTimeFormatter.format(x.getDateTime()) + " jest przyjęta").create();
+            reservationRepository.save(x);
         });
 
     }
@@ -46,6 +59,15 @@ public class ReservationServiceImplementation implements ReservationServiceInter
 
     @Override
     public void deleteReservation(Long id) {
+        reservationRepository.findById(id).ifPresent(reservation -> {
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm a");
+            Message.creator(
+                    new PhoneNumber(reservation.getAppUser().getPhoneNumber()),
+                    new PhoneNumber(twilioConfiguration.getTrialNumber()),
+                    "Twoja rezerwacja na: " + reservation.getSubCategory().getName() + " w dniu "
+                    + dateTimeFormatter.format(reservation.getDateTime()) + " była odwołana").create();
+            reservationRepository.delete(reservation);
+        });
 
     }
 
