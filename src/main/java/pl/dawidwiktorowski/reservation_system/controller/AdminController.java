18f80e7543573f5860b391281c511cd6c0e6bac5
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-21 01:17:20 +0100

    Added addSubcategory and addCategory endpoints

--- src/main/java/pl/dawidwiktorowski/reservation_system/controller/AdminController.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/controller/AdminController.java
@@ -5,8 +5,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.dawidwiktorowski.reservation_system.model.Category;
+import pl.dawidwiktorowski.reservation_system.model.SubCategory;
 import pl.dawidwiktorowski.reservation_system.service.CategoryServiceInterface;
 import pl.dawidwiktorowski.reservation_system.service.ReservationServiceInterface;
 import pl.dawidwiktorowski.reservation_system.service.SubCategoryServiceInterface;
@@ -26,16 +29,30 @@ public class AdminController {
 
     @PreAuthorize("isAuthenticated() and hasAuthority('ROLE_ADMIN')")
     @GetMapping(value = "/main")
-    public String showAdminPanel(Model model){
+    public String showAdminPanel(Model model) {
         List<Category> categories = categoryServiceInterface.findAll();
         model.addAttribute("categories", categories);
         return "admin/homePage";
     }
 
     @GetMapping(value = "/addCategory")
-    public String showAddCategoryForm(Model model){
+    public String showAddCategoryForm(Model model) {
         model.addAttribute("category", new Category());
         return "admin/addcategory";
+    }
+
+    @GetMapping(value = "/addSubCategory")
+    public String showAddSubcategoryForm(Model model) {
+        List<Category> categories = categoryServiceInterface.findAll();
+        model.addAttribute("categories", categories);
+        model.addAttribute("subcategory", new SubCategory());
+        return "admin/addsubcategory";
+    }
+
+    @PostMapping(value = "/addCategory")
+    public String addCategory(@ModelAttribute Category category) {
+        categoryServiceInterface.add(category);
+        return "redirect:/admin/main";
     }
 
 }
