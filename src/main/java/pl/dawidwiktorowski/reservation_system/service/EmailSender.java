251e5503106af808ef635af56d587eef9f717d94
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2022-01-03 23:51:15 +0100

    Added delete confirm reservation

--- src/main/java/pl/dawidwiktorowski/reservation_system/service/EmailSender.java
+++ src/main/java/pl/dawidwiktorowski/reservation_system/service/EmailSender.java
@@ -1,5 +1,6 @@
 package pl.dawidwiktorowski.reservation_system.service;
 
+import org.springframework.scheduling.annotation.Async;
 import pl.dawidwiktorowski.reservation_system.model.AppUser;
 import pl.dawidwiktorowski.reservation_system.model.SubCategory;
 
@@ -11,4 +12,7 @@ public interface EmailSender {
     void reservationRequestEmailSend(AppUser appUser, LocalDateTime dateTime, SubCategory subCategory);
 
     void sendEmailWhenUserUpdateReservation(AppUser appUser, LocalDateTime localDateTime, SubCategory subCategory);
+
+    @Async
+    void sendEmailWhenUserDeleteReservation(AppUser appUser, LocalDateTime localDateTime, SubCategory subCategory);
 }
