5c0cb34e4fff6b2c57b28038e7b95a4eae26542a
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2022-01-10 22:12:53 +0100

    Added add method tests

--- src/test/java/pl/dawidwiktorowski/reservation_system/service/impl/SubCategoryServiceImplementationTest.java
+++ src/test/java/pl/dawidwiktorowski/reservation_system/service/impl/SubCategoryServiceImplementationTest.java
@@ -3,10 +3,14 @@ package pl.dawidwiktorowski.reservation_system.service.impl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.dawidwiktorowski.reservation_system.model.SubCategory;
 import pl.dawidwiktorowski.reservation_system.repository.SubCategoryRepository;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 
@@ -15,19 +19,73 @@ class SubCategoryServiceImplementationTest {
 
     @Mock
     private SubCategoryRepository subCategoryRepository;
-    private SubCategoryServiceImplementation underTest;
+    private SubCategoryServiceImplementation subCategoryServiceImplementation;
+    private SubCategory subCategory;
 
     @BeforeEach
     void setUp() {
-        underTest = new SubCategoryServiceImplementation(subCategoryRepository);
+        subCategoryRepository = Mockito.mock(SubCategoryRepository.class);
+        subCategoryServiceImplementation = new SubCategoryServiceImplementation(subCategoryRepository);
+        subCategory = Mockito.mock(SubCategory.class);
     }
 
     @Test
     void findAll() {
         // when
-        underTest.findAll();
+        subCategoryServiceImplementation.findAll();
 
         //then
         verify(subCategoryRepository).findAll();
+    }
+
+    @Test
+    void shouldGetIdEquals1L() {
+        // given
+        Mockito.when(subCategory.getId()).thenReturn(1L);
+
+        // when
+        subCategoryServiceImplementation.add(subCategory);
+
+        // then
+        ArgumentCaptor<SubCategory> captor = ArgumentCaptor.forClass(SubCategory.class);
+        Mockito.verify(subCategoryRepository).save(captor.capture());
+
+        SubCategory subCategoryValue = captor.getValue();
+        assertThat(subCategoryValue.getId()).isEqualTo(1L);
+
+    }
+
+    @Test
+    void shouldGetNameEqualsRzesy() {
+        // given
+        Mockito.when(subCategory.getName()).thenReturn("Rzęsy");
+
+        // when
+        subCategoryServiceImplementation.add(subCategory);
+
+        // then
+        ArgumentCaptor<SubCategory> captor = ArgumentCaptor.forClass(SubCategory.class);
+        Mockito.verify(subCategoryRepository).save(captor.capture());
+
+        SubCategory subCategoryValue = captor.getValue();
+        assertThat(subCategoryValue.getName()).isEqualTo("Rzęsy");
+
+    }
+
+    @Test
+    void shouldGetPriceEquals_21_22() {
+        // given
+        Mockito.when(subCategory.getPrice()).thenReturn(21.22);
+
+        // when
+        subCategoryServiceImplementation.add(subCategory);
+
+        // then
+        ArgumentCaptor<SubCategory> captor = ArgumentCaptor.forClass(SubCategory.class);
+        Mockito.verify(subCategoryRepository).save(captor.capture());
+
+        SubCategory subCategoryValue = captor.getValue();
+        assertThat(subCategoryValue.getPrice()).isEqualTo(21.22);
+
     }
 }
