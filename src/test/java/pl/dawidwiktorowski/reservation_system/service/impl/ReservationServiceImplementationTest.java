e7e8f342045a2b954efa1844c36ec29ae7341d80
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2022-01-10 23:35:03 +0100

    Refactor test

--- src/test/java/pl/dawidwiktorowski/reservation_system/service/impl/ReservationServiceImplementationTest.java
+++ src/test/java/pl/dawidwiktorowski/reservation_system/service/impl/ReservationServiceImplementationTest.java
@@ -1,9 +1,11 @@
 package pl.dawidwiktorowski.reservation_system.service.impl;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import pl.dawidwiktorowski.reservation_system.config.TwilioConfiguration;
 import pl.dawidwiktorowski.reservation_system.model.AppUser;
 import pl.dawidwiktorowski.reservation_system.model.Reservation;
@@ -53,9 +55,7 @@ class ReservationServiceImplementationTest {
         reservationServiceImplementation.save(reservation, appUser);
 
         // then
-
         verify(reservationRepository).save(captor.capture());
-
         Reservation reservation = captor.getValue();
         assertThat(reservation.getId()).isEqualTo(3L);
 
@@ -70,26 +70,9 @@ class ReservationServiceImplementationTest {
         reservationServiceImplementation.save(reservation, appUser);
 
         // then
-
         verify(reservationRepository).save(captor.capture());
-
         Reservation reservation = captor.getValue();
         assertThat(reservation.isStatus()).isEqualTo(true);
 
-    }
-
-    @Test
-    void deleteReservation() {
-    }
-
-    @Disabled
-    @Test
-    void update() {
-        // given
-
-
-        // then
-
-        // when
     }
 }
