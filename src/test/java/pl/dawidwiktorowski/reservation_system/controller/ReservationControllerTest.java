a06a71cb38eec37d157548756e1f2e4254e73106
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2022-04-15 10:13:18 +0200

    Added admin panel reservation photo

---
+++ src/test/java/pl/dawidwiktorowski/reservation_system/controller/ReservationControllerTest.java
@@ -0,0 +1,18 @@
+package pl.dawidwiktorowski.reservation_system.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ReservationControllerTest {
+
+}
